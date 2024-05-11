9e25ebcd6c3a11434ca74bf38066b9c655d70b69
Gustavo <gustavoferraz405@gmail.com>
2024-05-11 16:10:47 -0300

    Firebase adicionado

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Importe o arquivo com as opções de inicialização do Firebase
 
 import 'app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Certifique-se de inicializar os widgets do Flutter
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform, // Use as opções de inicialização do Firebase do seu arquivo
+  );
   runApp(const App());
 }
